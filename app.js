f195d6d7d791209af74d0b6084eab5177d10bba4
migaman <mikee@bluewin.ch>
2018-07-20 21:51:04 +0200

    Load contracts ABI dynamically

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ var bodyparser = require('body-parser');
 var app = express();
 
 app.use(bodyparser.json());
-app.use(function(req, res, next) {
-    next();
+app.use(function (req, res, next) {
+	next();
 });
 app.use(express.static('./static'));
+app.use('/contracts', express.static('./build/contracts'));
+
 
 app.listen(9001);
